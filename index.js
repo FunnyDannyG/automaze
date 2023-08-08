cf8a3577380bd99b39d0d76f44e5217e8a9a0e04
RayTracerGC <141577659+RayTracerGC@users.noreply.github.com>
2023-08-08 14:16:01 -0300

    Add support for slash commands

--- index.js
+++ index.js
@@ -66,4 +66,50 @@ client.modelSearchEngine.addAll(documents);
     require(`./Handlers/${handler}`)(client, Discord);
 });
 
+// slash commands bruh
+client.slashCommands = new Discord.Collection();
+
+const fs = require('node:fs');
+const path = require('node:path');
+
+const foldersPath = path.join(__dirname, 'Commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('Slash.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.slashCommands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.on(Discord.Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+	
+    const command = interaction.client.slashCommands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
 client.login(process.env.token); // Import dotenv and make your own env file with the token in it
