cd662de6a7ad0ee313a13ad9f46fdb733e149acb
DeprecatedTable <110362433+DeprecatedTable@users.noreply.github.com>
2023-08-12 01:25:10 +0000

    added local deployment

---
+++ deploy-local-commands.js
@@ -0,0 +1,59 @@
+/*
+In the .env file, add both the id where the slash command will be registered and the bot client secret.
+If you are going to run this locally, un-comment dotenv import and config.
+
+const dotenv = require('dotenv');
+dotenv.config();
+*/
+
+const clientId = process.env.clientId;
+const token = process.env.token;
+
+const guildId = '1089076875999072296'; // By default, this is the guildId of AI Hub. If you want to deploy in your own guild locally, change this ID accordingly
+
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+
+let foldersPath = path.join(__dirname, 'CommandsSlash');
+let commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command && command.scope === 'local') {
+            commands.push(command.data.toJSON());
+        }
+        else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(token);
+
+// and deploy your commands!
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(
+            Routes.applicationGuildsCommands(clientId, guildId),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    }
+    catch (error) {
+        // And of course, make sure you catch and log any errors!
+        console.error(error);
+    }
+})();
