8c29d6ed98ff38923755a4197f83c16e211e20c8
Bastian Marin <bastyloko11@gmail.com>
2023-08-13 03:54:43 -0400

    JSON unification: Part I

--- Events/guildBanAdd.js
+++ Events/guildBanAdd.js
@@ -3,16 +3,16 @@ const { EmbedBuilder, AuditLogEvent } = require("discord.js");
 module.exports = {
     name: "guildBanAdd",
     once: false,
-    run(_, ban){
+    run(Client, ban){
 
         const banEmbed = new EmbedBuilder()
-        .setTitle(`${ban.user.username} has been banned!`)
-        .setColor(`Red`);
+            .setTitle(`${ban.user.username} has been banned!`)
+            .setColor(`Red`);
     
         ban.guild.fetchAuditLogs({type: AuditLogEvent.MemberBanAdd}).then(auditLog => {
             const banEntry = auditLog.entries.first();
             banEmbed.setDescription(`Executed by ${banEntry.executor} <t:${Math.round(Date.now() / 1000)}:R>`);
-            ban.guild.channels.cache.get('1127798056734179408').send({embeds: [banEmbed]});
+            ban.guild.channels.cache.get(Client.discordIDs.Admin.BanLog).send({embeds: [banEmbed]});
         });
 
     }
